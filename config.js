9cac533a13f703ad2b1aab364e9b68c2a721bfb8
cuncon123 <87967520+cuncon123@users.noreply.github.com>
2021-07-26 14:46:08 +0700

    Update config.js

--- config.js
+++ config.js
@@ -9,10 +9,10 @@ const CONFIG = {
     desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'làm người yêu anh nhé <33',
     btnNo: 'Không, Anh trai à :3',
-    question:'đố em biết trên bầu trời <3',
+    question:'đố em biết trên trời với anh ngôi sao nào là sáng nhất không<3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    reply: 'là em <33333333',
+    mess: 'đúng rồi 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'đừng giận anh nữa nhé, anh hông dám vậy nữa.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://drive.google.com/file/d/1AtT65CC31BcmNmiYciqnfGvx_-EmsGOu/view?usp=sharing'
